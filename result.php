379cf83da7be2352bae00f527516ee4bc7e918fe
SamerSenbol <samer.senbol@gmail.com>
2019-10-12 23:40:13 +0200

    Adding the result.php to show the data which the databas have.

--- result.php
+++ result.php
@@ -1,28 +1,41 @@
 <?php
-
+include "./includes/html-start.php";
 include_once 'db.php';
+
 ?>
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Nu</th>
+            <th>Email</th>
+            <th>Tel</th>
+            <th>Date</th>
+            <th>Ip Address</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php   
+        $sql = "SELECT * FROM users";
+        $users = mysqli_query($con,$sql);  
+        while($row = mysqli_fetch_assoc($users)) {
+            $Nu       = $row['Nu'];
+            $Email    = $row['Email'];
+            $Tel      = $row['Tel'];
+            $date     = $row['Date'];
+            $line     = $row['ipAddress'];
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+            echo "<tr>";
+            
+            echo "<td>$Nu </td>";
+            echo "<td>$Email </td>";
+            echo "<td>$Tel</td>";
+            echo "<td>$date</td>";
+            echo "<td>$line</td>";
 
-        $sql="SELECT * FROM users;";
-        $data = mysqli_query($con,$sql);
-        if($data){
-            echo  $data;
+            echo "</tr>";
         }
-        else{
-        echo "No";
-        }
-    ?>
-    
-</body>
-</html>
+        ?>
+    </tbody>
+</table>
+<?php
+include "./includes/html-end.php";
