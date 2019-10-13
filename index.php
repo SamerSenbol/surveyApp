382283a3ae62d16aa12dfdd61e70256dd43b90b0
SamerSenbol <samer.senbol@gmail.com>
2019-10-13 13:56:45 +0200

    Adding the FB share button.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ include "./includes/html-start.php";
 ?>
     <div class="container">
         <div class="row justify-content-md-center">
-            <h3>ياترى في حدا متلى حابب يشتغل كشوفير سيارة شحن او باص بس في كتير عوائق عم تواجهو؟</h3>
-            <h3>خلينا نوصل صوتنا للجهات المعنية بالسويد حتى يلاقولنا حل ويساعدونا</h3>
+            <h5>ياترى في حدا متلى حابب يشتغل كشوفير سيارة شحن او باص بس في كتير عوائق عم تواجهو؟</h5>
+            <h5>خلينا نوصل صوتنا للجهات المعنية بالسويد حتى يلاقولنا حل ويساعدونا</h5>
             <form action="insert.php" method="POST">
                 <div class="form-group"> <br><br>
                     <h6>اذا كنت ترغب بمتابعة كل مايتعلق بهذا الشأن يمكنك ترك رقمك او ايميلك وسنعلمك بكل جديد</h6>
@@ -23,9 +23,16 @@ include "./includes/html-start.php";
         </div>
         <div class="row justify-content-md-center">
             <div class="whatsup">
-                <h6>شارك التصويت مع الأصدقاء عبر الواتس أب</h6>
+                <h6>لمشاركة التصويت مع الأصدقاء عبر الواتس أب</h6>
                 <a data-text="شارك فصوتك يحدث فرقاً" data-link="http://www.doitonline.se/" class="whatsapp w3_whatsapp_btn w3_whatsapp_btn_large">مشاركة</a>
             </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div id="fb-root"></div>
+                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0&appId=356632788307657&autoLogAppEvents=1"></script>
+            </div>
+            <h6>لمشاركة التصويت مع الأصدقاء عبر الفيس بوك </h6>
+            <div class="fb-share-button" data-href="http://www.doitonline.se/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.doitonline.se%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">مشاركة</a></div>
         </div>
     </div>
     
